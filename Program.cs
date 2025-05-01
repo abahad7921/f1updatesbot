1f0ffb7008fb72ef6b4c74fe628cf0e90b1819fe
Abdul Ahad <55959138+AbdulAhad7860@users.noreply.github.com>
2025-05-02 00:37:41 +0530

    init commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using F1UpdatesBot.Src.Services;
+using Newtonsoft.Json;
+
+namespace F1UpdatesBot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var sessionService = new SessionsService();
+            //var res = await sessionService.getAll();
+            //Console.WriteLine(JsonConvert.SerializeObject(res));
+
+            var res1 = await sessionService.getCurrentSessionKey();
+            Console.Write(JsonConvert.SerializeObject(res1));
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
