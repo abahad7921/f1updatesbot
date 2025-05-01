c26c26012cba7f831d89bb00262ae50d6925246a
Abdul Ahad <55959138+AbdulAhad7860@users.noreply.github.com>
2025-05-02 00:39:06 +0530

    create sessionservice

---
+++ Src/Services/Interfaces/ISessionsService.cs
@@ -0,0 +1,16 @@
+﻿using F1UpdatesBot.Src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1UpdatesBot.Src.Services.Interfaces
+{   
+    public interface ISessionsService
+    {
+        Task<List<Session>> getAll();
+        Task<int> getCurrentSessionKey();
+    }
+}
+
---
+++ Src/Services/SessionsService.cs
@@ -0,0 +1,41 @@
+﻿using F1UpdatesBot.Src.Models;
+using F1UpdatesBot.Src.Services.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1UpdatesBot.Src.Services
+{
+    public class SessionsService : ISessionsService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SessionsService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.openf1.org/v1/")
+            };
+        }
+
+        public async Task<List<Session>> getAll()
+        {
+            var response = await _httpClient.GetStringAsync("sessions?year=2025&session_name=Race");
+            return JsonConvert.DeserializeObject<List<Session>>(response);
+        }
+
+        public async Task<int> getCurrentSessionKey()
+        {
+            var sessions = await getAll();
+            var upcomingRace = sessions
+                .Where(s => s.SessionType == "Race" && s.DateStart > DateTime.UtcNow)
+                   .OrderBy(s => s.DateStart)
+                .FirstOrDefault();
+
+            return upcomingRace == null ? 0 : upcomingRace.SessionKey;
+        }
+    }
+}
