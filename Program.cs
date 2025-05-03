1745ab8586525a49b62f06825449c9a6536bced4
Abdul Ahad <55959138+AbdulAhad7860@users.noreply.github.com>
2025-05-04 00:51:56 +0530

    inject services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using Discord;
+using F1UpdatesBot.Src;
 using F1UpdatesBot.Src.Services;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
+using F1UpdatesBot.Src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.WebSocket;
+using F1UpdatesBot.Src.Services.Interfaces;
+using F1UpdatesBot.Src.Models;
 
 namespace F1UpdatesBot
 {
@@ -9,13 +22,35 @@ namespace F1UpdatesBot
         public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddHttpClient<Openf1Service>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.openf1.org/v1/");
+        });
 
-            var sessionService = new SessionsService();
-            //var res = await sessionService.getAll();
-            //Console.WriteLine(JsonConvert.SerializeObject(res));
+        services.AddSingleton(provider =>
+        {
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.Guilds | GatewayIntents.GuildMessages
+            };
+            return new DiscordSocketClient(config);
+        });
 
-            var res1 = await sessionService.getCurrentSessionKey();
-            Console.Write(JsonConvert.SerializeObject(res1));
+        services.AddSingleton<SessionsService>();
+        services.AddSingleton<LapUpdateService>();
+        services.AddSingleton<RaceResultService>();
+        services.AddSingleton<SchedulerService>();
+        services.AddSingleton<DriverService>();
+        services.AddSingleton<Bot>();
+    })
+    .Build();
+
+
+            var bot = host.Services.GetRequiredService<Bot>();
+            await bot.RunAsync();
         }
 
         private static Task Log(LogMessage msg)
